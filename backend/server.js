40ae4fe84021a5dd51565fca6d88d42159a03de5
Stefan <stefanreimer777@gmail.com>
2021-08-26 11:16:11 +0200

    updated messagecontroller

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 app.use(cors());
-const initializePassport = require("./passport-config");
+const initializePassport = require('./passport-config');
 initializePassport(passport);
 
 // ejs
@@ -13,7 +13,6 @@ const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,18 +26,18 @@ mongoose
     }
   )
   .then(() => {
-    console.log("we are connected to the database. 👍");
+    console.log('we are connected to the database. 👍');
   })
   .catch((error) => {
-    console.log("an error occurred while connecting ot the db", error);
+    console.log('an error occurred while connecting ot the db', error);
   });
 
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require('./routes/userRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
-
-app.use("/user", userRoutes);
-
+app.use('/user', userRoutes);
+app.use('/contact', messageRoutes);
 
 app.listen(4001, () => {
-  console.log("the webserver is running on port 4001 💚 💌");
+  console.log('the webserver is running on port 4001 💚 💌');
 });
