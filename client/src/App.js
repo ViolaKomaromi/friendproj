fdbc716d59173898bdfe5cc6ce9582fecb8df5c8
Stefan <stefanreimer777@gmail.com>
2021-09-01 09:37:03 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import Profile from './components/Profile/Profile';
-import FriendsList from './components/Profile/FriendsList';
-import MailBox from './components/Profile/MailBox';
+
+import Profile from './components/Profile/Profile-settings';
 
 function App() {
   return (
     <>
       <Profile />
-      <MailBox />
-      <FriendsList />
     </>
   );
 }
