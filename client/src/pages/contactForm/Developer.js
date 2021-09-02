3f24170bcc7e43007ae1853f8fc7a18bf843730f
LiamTimlin <williamtimlin@outlook.com>
2021-09-02 11:26:39 +0200

    updated landpage css, added contact form to contact popup, add developer contact components

---
+++ client/src/pages/contactForm/Developer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Developer() {
+    return (
+        <div>
+            <p>Name</p>
+            <p>Github</p>
+            <p>Email</p>
+        </div>
+    );
+}
+
+export default Developer;
