a57bd2facd682a729b3b000f2e74f20711178e2a
Stefan <stefanreimer777@gmail.com>
2021-09-09 11:00:50 +0200

    another css update

--- client/src/components/404-page/404-page.js
+++ client/src/components/404-page/404-page.js
@@ -1,40 +1,73 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import image from './54a170c2dd0895dd408b4592.jpeg';
-import './404.css';
+//import './404.css';
 
 const PageNotFound = () => {
   return (
-    <div class='container'>
-      <div class='row'>
-        <div class='col-md-12'>
-          <div class='error-template'>
-            <img src={image} alt='Admin' />
-            {/* <img src='https://i.imgur.com/qIufhof.png' /> */}
-            <h1>Oops!</h1>
-            <h2>404 Not Found</h2>
-            <div class='error-details'>
-              Sorry, an error has occured, Requested page not found!
-            </div>
-            <div class='error-actions'>
+    // <div style={{ backgroundImage: `url(${image})` }}>Hello World</div>
+
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-md-12 col-sm-12'>
+          <img src={image} alt='Admin' />
+          <div
+            className='card shadow-lg border-0 rounded-lg mt-5 mx-auto'
+            // style='width: 30rem;'
+          >
+            <h3 className='card-header display-1 text-muted text-center'>
+              404
+            </h3>
+
+            <span className='card-subtitle mb-2 text-muted text-center'>
+              Page Could Not Be Found
+            </span>
+
+            <div className='card-body mx-auto'>
               <a
-                href='http://www.jquery2dotnet.com'
-                class='btn btn-primary btn-lg'
+                type='button'
+                href='/'
+                className='btn btn-sm btn-primary text-white'
               >
-                <span class='glyphicon glyphicon-home'></span>
-                Take Me Home{' '}
-              </a>
-              <a
-                href='http://www.jquery2dotnet.com'
-                class='btn btn-default btn-lg'
-              >
-                <span class='glyphicon glyphicon-envelope'></span> Contact
-                Support{' '}
+                Back To Home
               </a>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    // <div className='container'>
+    //   <div className='row'>
+    //     <div className='col-md-12'>
+    //       <div className='error-template'>
+    //         <img src={image} alt='Admin' />
+    //         {/* <img src='https://i.imgur.com/qIufhof.png' /> */}
+    //         <h1>Oops!</h1>
+    //         <h2>404 Not Found</h2>
+    //         <div className='error-details'>
+    //           Sorry, an error has occured, Requested page not found!
+    //         </div>
+    //         <div className='error-actions'>
+    //           <a
+    //             href='http://www.jquery2dotnet.com'
+    //             className='btn btn-primary btn-lg'
+    //           >
+    //             <span className='glyphicon glyphicon-home'></span>
+    //             Take Me Home{' '}
+    //           </a>
+    //           <a
+    //             href='http://www.jquery2dotnet.com'
+    //             className='btn btn-default btn-lg'
+    //           >
+    //             <span className='glyphicon glyphicon-envelope'></span> Contact
+    //             Support{' '}
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
 
     // <div id='wrapper'>
     //   {/* <img src='https://i.imgur.com/qIufhof.png' /> */}
