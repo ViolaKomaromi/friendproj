1051d933f66455131045636619ec81cd4eb341ee
Viola <violaristic@yahoo.com>
2021-09-09 14:40:37 +0200

    start of the backend

--- client/src/components/Match/Match.js
+++ client/src/components/Match/Match.js
@@ -1,7 +1,36 @@
 
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-export default function Match() {
+import axios from '../../util/axios';
+import FormWrapper from "../Wrapers/wraper";
+import mockData from "../../../../backend/MOCK_DATA(2).json";
+
+
+
+export default function randomMatch() {
+
+    const [username, setUsername] = useState('');
+
+
+    useEffect(() => {
+
+        // axios.get('../../../../backend/MOCK_DATA(2).json')
+        // .then (res => console.log(res))
+        // .catch (err => console.log(err))
+        axios.get('/random'  )
+        .then ((res )=> {
+                console.log(res);
+                setUsername(res.data.username);
+        })
+        .catch (err => console.log(err))
+
+    })
+    const history = useHistory();
+
+
+
     return (
         <div>
             
