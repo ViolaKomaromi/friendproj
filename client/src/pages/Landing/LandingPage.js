26f768f2d085aaeca7e15704fe0b4073d7972b31
Viola <violaristic@yahoo.com>
2021-09-22 11:16:30 +0200

    fixed it :)

--- client/src/pages/Landing/LandingPage.js
+++ client/src/pages/Landing/LandingPage.js
@@ -1,27 +1,33 @@
 import React from "react";
-import Nav from "../../components/Header/LandingNav";
+import Nav from "../../components/Nav/Navbar";
 import SignUp from "../LandingTwo/SignUp";
 import Login from "../LandingTwo/Login";
-import "../Landing/landingPage.css";
+import "./landingPage.css";
 import Carousel from "../../components/Carousel/Carousel";
+import landingImage from "../../image/landing-background.png";
 
 function LandingPage() {
     return (
         <>
             <Nav />
             <main>
-                <header>
-                    <h2>
-                        Find a <br /> <span style={{ fontSize: "9rem" }}>German</span> <br /> Friend
-                    </h2>
-                </header>
-                <section>
-                    <h3>based on you</h3>
-                    <div className="sign-login-btns">
-                        <SignUp />
-                        <Login />
-                    </div>
-                </section>
+                <div className="overlay"></div>
+                <div className="landingWrapper">
+                    <header>
+                        <h2>
+                            Find a <br /> <span style={{ fontSize: "9rem" }}>German</span> <br /> Friend
+                        </h2>
+
+                        <h3>based on you</h3>
+                        <div className="underline"></div>
+                    </header>
+                    <section>
+                        <div className="sign-login-btns">
+                            <SignUp />
+                            <Login />
+                        </div>
+                    </section>
+                </div>
             </main>
             <div>
                 <Carousel />
