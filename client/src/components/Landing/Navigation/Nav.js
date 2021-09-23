cac3b4039813a8eb79b4b82dc5f12ee181bd3661
Alagie Abdoulie Drammeh <unrulyfitruly@gmail.com>
2021-09-23 16:00:36 +0200

    merged changes

--- client/src/components/Landing/Navigation/Nav.js
+++ client/src/components/Landing/Navigation/Nav.js
@@ -2,32 +2,21 @@
 // import axios from "../../../util/axios";
 
 import React from "react";
-<<<<<<< HEAD
-import "../../Landing/Navigation/landingNav.css";
-import { BrowserRouter, Route, Link, useHistory } from "react-router-dom";
-=======
 import AboutPage from "../About/AboutPage";
 import ContactPage from "../Contact/ContactPage";
 import "../Navigation/";
 import logo from "../../images/FJ_friendja.png";
->>>>>>> Match-frontend-merge-clone
 
 function Nav(props) {
-    const history = useHistory();
+    
     return (
         <div>
             <nav className="navbar fixed-top navbar-light  navbar-expand-lg ">
                 <div className="container-fluid ">
-<<<<<<< HEAD
-                    <button className="navbar-brand" href="#">
-                        FriendJa Logo
-                    </button>
-=======
                     {/* <a className="navbar-brand" href="#">
                         FriendJa Logo
                     </a> */}
                     <img src={logo} alt="" />
->>>>>>> Match-frontend-merge-clone
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -40,13 +29,6 @@ function Nav(props) {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
-<<<<<<< HEAD
-                        <div className="navbar-nav ms-auto ">
-                            <button className="nav-link" href="#">
-                                About
-                            </button>
-                            <button onClick={() => history.push('/contact/message')}>Contact</button>
-=======
                         <div className="navbar-nav ms-auto">
                             {/* <a className="nav-link" href="#">
                                 About
@@ -58,7 +40,6 @@ function Nav(props) {
                                 <AboutPage />
                                 <ContactPage />
                             </div>
->>>>>>> Match-frontend-merge-clone
                         </div>
                     </div>
                 </div>
