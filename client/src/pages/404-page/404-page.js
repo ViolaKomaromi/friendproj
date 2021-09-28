36793b67b9673fde657af335c2f0173133dc99b7
Stefan <stefanreimer777@gmail.com>
2021-09-28 18:26:33 +0200

    404-page css fixed

--- client/src/pages/404-page/404-page.js
+++ client/src/pages/404-page/404-page.js
@@ -5,31 +5,54 @@ import image from './54a170c2dd0895dd408b4592.jpeg';
 
 const PageNotFound = () => {
   return (
-    // <div style={{ backgroundImage: `url(${image})` }}>Hello World</div>
+    // <div className='container'>
+    //   <div className='row justify-content-center'>
+    //     <div className='col-md-12 col-sm-12'>
+    //       <img src={image} alt='Admin' />
+    //       <div
+    //         className='card shadow-lg border-0 rounded-lg mt-5 mx-auto'
+    //         // style='width: 30rem;'
+    //       >
+    //         <h3 className='card-header display-1 text-muted text-center'>
+    //           404
+    //         </h3>
+
+    //         <span className='card-subtitle mb-2 text-muted text-center'>
+    //           Page Could Not Be Found
+    //         </span>
+
+    //         <div className='card-body mx-auto'>
+    //           <a
+    //             type='button'
+    //             href='/'
+    //             className='btn btn-sm btn-primary text-white'
+    //           >
+    //             Back To Home
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
 
     <div className='container'>
-      <div className='row justify-content-center'>
-        <div className='col-md-12 col-sm-12'>
-          <img src={image} alt='Admin' />
-          <div
-            className='card shadow-lg border-0 rounded-lg mt-5 mx-auto'
-            // style='width: 30rem;'
-          >
-            <h3 className='card-header display-1 text-muted text-center'>
-              404
-            </h3>
-
-            <span className='card-subtitle mb-2 text-muted text-center'>
-              Page Could Not Be Found
-            </span>
-
-            <div className='card-body mx-auto'>
-              <a
-                type='button'
-                href='/'
-                className='btn btn-sm btn-primary text-white'
-              >
-                Back To Home
+      <img src={image} alt='Admin' />
+      <div className='row'>
+        <div className='col-md-12'>
+          <div className='error-template'>
+            <h1>Oops!</h1>
+            <h2>404 Not Found</h2>
+            <div className='error-details'>
+              Sorry, an error has occured, Requested page not found!
+            </div>
+            <div className='error-actions'>
+              <a href='/' className='btn btn-primary btn-lg'>
+                <span className='glyphicon glyphicon-home'></span>
+                Take Me Home{' '}
+              </a>
+              <a href='/' className='btn btn-primary btn-lg'>
+                <span className='glyphicon glyphicon-envelope'></span> Contact
+                Support{' '}
               </a>
             </div>
           </div>
