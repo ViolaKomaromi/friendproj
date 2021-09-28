dcd00b9d551fce25ea940335da720e865eb9c4db
AnneliTel <annelitelling@gmail.com>
2021-09-28 11:45:49 +0200

    updated my data for the developersjs

--- client/src/data/developers.js
+++ client/src/data/developers.js
@@ -9,9 +9,9 @@ const menu = [
     {
         id: 2,
         img: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-        name: "Anneli",
-        github: "breakfast",
-        email: "A-email",
+        name: "Anneli Telling",
+        github: "https://github.com/AnneliTel",
+        email: "annelitelling@gmail.com",
     },
     {
         id: 3,
