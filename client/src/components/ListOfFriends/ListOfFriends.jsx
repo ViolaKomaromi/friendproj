7beefe39dc34a9f8c908e5dc45d27e87aca93b4d
AnneliTel <annelitelling@gmail.com>
2021-09-29 00:00:56 +0200

    friendlist style touchups & other

--- client/src/components/ListOfFriends/ListOfFriends.jsx
+++ client/src/components/ListOfFriends/ListOfFriends.jsx
@@ -3,23 +3,24 @@ import "./ListOfFriends.css";
 import * as Icon from "react-bootstrap-icons";
 
 
-const ListOfFriends = ({ id, full_name, location, image }) => {
+const ListOfFriends = ({ id, full_name, location, image, friendcount }) => {
     return (
         <div>
             <section className="imagebox w3-button">
                 <div>
-                    <img src={`${image}`} alt={full_name} className="image" />
+                    <img src={`${image}`} alt={full_name} className="frlist-image" />
                 </div>
-                <div className="imagetext">
-                    <h3 className="full_name">{full_name}</h3>
+                <div className="fr-imagetext">
+                    <h3 className="friends_full_name">{full_name}</h3>
                     <p className="location-friendlist">{location}</p>
-                    
-                    
+                </div>
+                <div className="friendcount-box">
+                    <h4 className="friendcount">{friendcount}</h4>
                 </div>
                 <div>
-                <Icon.ChatLeftTextFill size={30} className="card-footer-btn" />
+                <Icon.ChatLeftTextFill size={30} className="card-footer-btn " />
                 <br />
-                    <Icon.XOctagonFill size={30} className="card-footer-btn" />
+                    <Icon.XOctagonFill size={30} className="card-footer-btn " />
                 </div>
             </section>
         </div>
--- client/src/data/developers.js
+++ client/src/data/developers.js
@@ -9,10 +9,11 @@ const menu = [
     },
     {
         id: 2,
-        img: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        img: "../image/anneli.png",
         name: "Anneli Telling",
         github: "https://github.com/AnneliTel",
         email: "annelitelling@gmail.com",
+        role: "Frontend"
     },
     {
         id: 3,
