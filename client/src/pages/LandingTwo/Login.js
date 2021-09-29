1fffa30d65fe04e0ae008cd627f4c7a1e5e7708b
Viola <violaristic@yahoo.com>
2021-09-29 11:48:37 +0200

    Thank you Gilles

--- client/src/pages/LandingTwo/Login.js
+++ client/src/pages/LandingTwo/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
             if (res.status == 200) {
                 console.log("yaaay the user has signed! 🟢");
                 handleLogin(true, res.data.token);
-                history.push("/random");
+                history.push("/");
                 console.log(res.data.token);
             }
         } catch (error) {
