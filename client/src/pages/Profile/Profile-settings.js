904a858e78e31bfcabcb0767c54907491c500570
Viola <violaristic@yahoo.com>
2021-09-29 09:30:48 +0200

    celaned it

--- client/src/pages/Profile/Profile-settings.js
+++ client/src/pages/Profile/Profile-settings.js
@@ -12,74 +12,7 @@ export default function Profile({ profile }) {
 
 
 
-    // return (
-    //     <div className="profile-page1">
-    //         <Navbar />
-    //         <div className="container">
-    //             <h2 className="profile-page-title">Your Profile</h2>
-    //             <div className="main-body1">
-    //                 <div className="card1 profile-setting-cards card-body ">
-    //                     <div className="card1">
-    //                         <h5 className="main-body-title1"></h5>
-    //                         <div className="d-flex flex-column align-items-center text-center">
-    //                             <img src={logo} alt="Admin" className="rounded-circle" width="200" />
-    //                             <div className="row" >
-    //                                 <div className="col-sm-12">
-    //                                     <a className="btn profile-settings-submit " target="__blank" >
-    //                                         Choose Image
-    //                                     </a>
-    //                                 </div>
-    //                             </div>
-    //                             <div className="mt-3">
-    //                                 <div className="row1">
-    //                                     <div className="col-sm-12">
-    //                                         <form className="form-inline">
-    //                                             <label className="my-1 mr-2" for="inlineFormCustomSelectPref">
-    //                                                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil delectus dolorum perspiciatis quisquam cupiditate assumenda, nisi quos ducimus doloremque dignissimos.
-    //                                             </label>
 
-    //                                         </form>
-    //                                     </div>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-
-    //                 <div className="card profile-setting-cards card-body">
-    //                     <div className="card1">
-    //                         <h3 className="main-body-title1">About me</h3>
-    //                          <p> I love to paint, read, travel and meet new people. </p>
-    //                          <p>I live in Germany 6 months, would be awsome if i meet local people to improve my language skills and to have fun.  </p>
-    //                     </div>
-    //                 </div>
-
-    //                 <div className="card1 card-body">
-    //                     <div className="card profile-setting-cards">
-    //                         <h3 className="main-body-title">My Details</h3>
-    //                         <div className="profile-settings-forms1">
-    //                          <h4>Name</h4>
-    //                          <p>Viola Komaromi</p>
-    //                          <h4>City</h4>
-    //                          <p>Düsseldorf</p>
-    //                          <h4>Hobies</h4>
-    //                          <p>Reading, jogging, </p>
-    //                          <h4>Fun Fact about me</h4>
-    //                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, exercitationem?</p>
-    //                          <h4>My favorite quote</h4>
-    //                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores similique consequuntur sequi amet eaque? Quas, iste. Itaque vero ipsa enim?</p>
-
-
-
-
-
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
 
     return (
         <>
